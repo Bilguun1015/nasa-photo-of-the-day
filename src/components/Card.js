96c74f6547bf52a9641a57aa20418f562f7d9010
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-07-18 12:04:24 -0500

    stretch problem is working

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,19 +7,25 @@ import ParagraphDiv from "./ParagraphDiv"
 
 const Card = () => {
     const [data, setImage] = useState([]);
-    const [date, setDate] = useState('2017-07-13');
+    const [date, setDate] = useState('2019-07-17');
+    function changeDate(e){
+        e.preventDefault();
+        let userDate = e.target[0].value;
+        console.log(userDate)
+        setDate(userDate)
+    }
 
     useEffect( () => {
         axios
-            .get(`https://api.nasa.gov/planetary/apod?api_key=S8I4hQT1Gkj1cNOEGmgUCcBRsFG4dpIIFm5rryyy&date=2017-07-13`)
+            .get(`https://api.nasa.gov/planetary/apod?api_key=S8I4hQT1Gkj1cNOEGmgUCcBRsFG4dpIIFm5rryyy&date=${date}`)
             .then(response => {
                 const img = response.data
                 setImage(img);
             })
-    },[]);
+    }, [date]);
     return (
      <div>
-        <DateInput />  
+        <DateInput changeDate = {changeDate}/>  
         <TitleDiv title={data.title}/>
         <ImageDiv image = {data.url}/>
         <ParagraphDiv parag={data.explanation}/>
